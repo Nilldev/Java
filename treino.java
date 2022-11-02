bac85a1f5ce8df38630e84605109c77be1a96448
NillDev <68431145+Nilldev@users.noreply.github.com>
2022-11-02 18:24:52 -0300

    agendaPoo

---
+++ treino.java
@@ -0,0 +1,16 @@
+public class treino{
+    public static void main(String[] args) {
+        
+    
+    Contato C = new Contato("Evanildo Souza", "+55 99 652485145");
+
+   
+    C.print();
+    }
+}
+
+
+
+    
+
+    
